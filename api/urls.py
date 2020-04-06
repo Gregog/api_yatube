ebd1a82cf7d51725529c5452c57ca395b435aef2
Илья <idzaoshang@yandex.ru>
2020-04-07 02:49:46 +0300

    little url fix

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,13 @@ from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('api/v1/posts', PostViewSet)
+router.register('v1/posts', PostViewSet, basename="posts")
+
 
 urlpatterns = [
-    path('api/v1/posts/<int:post_id>/comments/', APICommentView.as_view()),
-    path('api/v1/posts/<int:post_id>/comments/<int:comment_id>/',
+    path('v1/posts/<int:post_id>/comments/', APICommentView.as_view()),
+    path('v1/posts/<int:post_id>/comments/<int:comment_id>/',
          APICommentDetailView.as_view()),
     path('', include(router.urls)),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
