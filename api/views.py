e7be2ef851f4102b5411073b2e2c7f7abc456e25
Илья <idzaoshang@yandex.ru>
2020-04-07 12:34:40 +0300

    BEST

--- api/views.py
+++ api/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.decorators import action
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .permissions import IsOwnerOrReadOnly
 from posts.models import Post, Comment
